f34db9798f68c4579fc6c2bc046373caf78aee12
Ralph Molu <rmolu11@gmail.com>
2024-03-01 11:14:21 -0500

    completed all functions, need to fix the following bugs: playerscore is not incremented, no leaderboard page/section yet, no condition for if the quiz is finished before the time is up. q

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -52,34 +52,37 @@ var questions = [
 ];
 
 
-//variables to get a hold of the questions and the answers
-var questionEl = document.getElementById("question-answer");
+//variables to get a hold of HTML elements
+var questionEl = document.getElementById("question");
+var questionContainer = document.getElementById("question-container");
 var answerEl = document.getElementById("btn-answer");
+var startButton = document.getElementById("btn-start-quiz");
 var introPage = document.getElementById("intro-page");
 var timerEl = document.getElementById("timer");
+var answerButtons = document.getElementById("answer-options");
+var resultEl = document.getElementById("results");
+var playerScore;
 var timeLeft = 60;
+var TimeId;
 
+startButton.addEventListener("click", startQuiz);
 
 var currentQuestionIndex = 0; // questions are stored in array. It's best to start at 0
-var playerScore = 0; // initiallize score to 0
 
 function startQuiz(){
-    //reset question
-    var currentQuestionIndex = 0;
-    //reset score
-    var playerScore = 0;
-    //display question
-    questionEl.classList.remove("hidden");
-    //hide intro page
+    var playerScore = 0; // initiallize score to 0
+    currentQuestionIndex = 0
+    questionContainer.classList.remove("hidden");
     introPage.classList.add("hidden");
     startTimer();
-    showNextQuestion();
+    console.log("you started the quiz");
+    showQuestion();
 }
 
 
 function startTimer(){
     timerEl.textContent = "Timer: "+ timeLeft;
-    var timerId = setInterval(function(){
+    timerId = setInterval(function(){
         timeLeft--;
         timerEl.textContent = "Timer: "+ timeLeft;
         if (timeLeft <= 0){
@@ -90,9 +93,69 @@ function startTimer(){
 
 }
 
-function showNextQuestion(){
-    var currentQuestion = questions[currentQuestionIndex];
-    //display question
-    questionEl.innerHTML = currentQuestion.question;
-    //display answers
+function showQuestion(){
+    answerButtons.innerHTML = "";
+    questionEl.innerHTML = questions[currentQuestionIndex].question;
+
+    for(var i = 0; i<questions[currentQuestionIndex].answers.length; i++){
+        var answerButton = document.createElement("button");
+        var answer = questions[currentQuestionIndex].answers[i];
+        answerButton.innerHTML = (i+1)+ "." + answer.text;
+        answerButton.classList.add("button");
+
+        answerButton.addEventListener("click", function(event){
+            chooseAnswer(event);
+        });
+
+        answerButtons.appendChild(answerButton);
+    }
+
+    answerButtons.classList.remove("hidden");
+
+
+}
+
+function chooseAnswer(event){
+    var chosenButton = event.target;
+    var answers = questions[currentQuestionIndex].answers;
+    // using the find() method on arrays to return the first element that matches the condition
+    var correctAnswer = answers.find(function(answer){ 
+        return answer.correct;
+    });
+
+    if (chosenButton.innerText === correctAnswer.innerText){
+        playerScore += 10;
+    } else {
+        timeLeft -= 10;
+    }
+
+    currentQuestionIndex ++;
+    if (currentQuestionIndex < questions.length){
+        showQuestion();
+    }else {
+        stopQuiz();
+    }
+}
+
+function stopQuiz(){
+    clearInterval(timerId);
+    //stop displaying the questions
+    questionContainer.classList.add("hidden");
+
+    //initials box
+    document.getElementById("all-done").classList.remove("hidden");
+    var submitBtn = document.getElementById("submit-btn");
+    var userInitialsInput = document.getElementById("user-initials");
+
+    submitBtn.onclick = function() {
+        var userInitials = userInitialsInput.value;
+        //check that the user enters their initials
+        if (userInitials){
+            document.getElementById("results-heading").innerHTML = "Good Job, " + userInitials + " you're all done! Your final score is: " + playerScore;
+            resultEl.classList.remove("hidden");
+        } else {
+            alert("Please input your initials");
+        }
+    }
+
 }
